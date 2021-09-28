0b834ac0476d0ed26057c36f5ceccf94b7b3ab44
djli15736 <djli15736@gmail.com>
2021-09-28 17:29:29 -0400

    made interface display my data

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import POWERS from './assets/powers.json';
 import { PowerViewer } from './components/PowerViewer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Container, Row } from 'react-bootstrap';
+import { Power } from './interfaces/power';
+
 
 
 function App(): JSX.Element {
+  const [points, setPoints] = useState(100); 
+ 
   return (
     <Container className="App">
       <Row>
       <Col>
-      <PowerViewer></PowerViewer>
-      <PowerViewer></PowerViewer>
-      <PowerViewer></PowerViewer>
+      <PowerViewer power={POWERS[0]}></PowerViewer>
+      <PowerViewer power={POWERS[1]}></PowerViewer>
+      <PowerViewer power={POWERS[2]}></PowerViewer>
       </Col>
       <Col>
-      <PowerViewer></PowerViewer>
-      <PowerViewer></PowerViewer>
-      <PowerViewer></PowerViewer>
+      <PowerViewer power={POWERS[3]}></PowerViewer>
+      <PowerViewer power={POWERS[4]}></PowerViewer>
+      <PowerViewer power={POWERS[5]}></PowerViewer>
       </Col>
       <Col>
-      <PowerViewer></PowerViewer>
-      <PowerViewer></PowerViewer>
-      <PowerViewer></PowerViewer>
+      <PowerViewer power={POWERS[6]}></PowerViewer>
+      <PowerViewer power={POWERS[7]}></PowerViewer>
+      <PowerViewer power={POWERS[8]}></PowerViewer>
       </Col>
       </Row>
     </Container>
--- src/components/PowerViewer.tsx
+++ src/components/PowerViewer.tsx
@@ -1,6 +1,20 @@
-import { Button, Col } from 'react-bootstrap';
+import { Button, Card as BootstrapCard, Col } from 'react-bootstrap';
+import { Power } from '../interfaces/power';
 
 
-export function PowerViewer(): JSX.Element {
-    return <Col><Button>Powers</Button></Col>
+export function PowerViewer({power}: {power: Power}): JSX.Element {
+    return <Col>
+    <BootstrapCard>
+            <BootstrapCard.Body>
+                <BootstrapCard.Title>{power.Name}</BootstrapCard.Title>
+                <BootstrapCard.Text>
+                    <strong>Description:</strong> {power.Description}
+                </BootstrapCard.Text>
+                <BootstrapCard.Text>
+                    <strong>Cost:</strong> {power.Cost}
+                </BootstrapCard.Text>
+                <Button>Select</Button> 
+            </BootstrapCard.Body>
+        </BootstrapCard>
+    </Col>
 }
